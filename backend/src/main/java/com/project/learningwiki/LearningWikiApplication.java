f6fc37aefffbb3f492095a6c0b0f6539355656bf
Ion-Radu PĂTRĂŞCOIU (101349) <ion_radu.patrascoiu@stud.acs.upb.ro>
2022-02-28 20:27:55 +0200

    First commit - backend initialization

---
+++ backend/src/main/java/com/project/learningwiki/LearningWikiApplication.java
@@ -0,0 +1,13 @@
+package com.project.learningwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningWikiApplication.class, args);
+	}
+
+}
